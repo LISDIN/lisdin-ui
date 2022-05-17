340311623fed47b7646fd55965ac583f2ca68c41
Mohammad Khan <mhrkhan13@gmail.com>
2022-05-17 18:20:32 -0400

    feat: authentication

---
+++ lisdin-ui/src/App.js
@@ -0,0 +1,13 @@
+import {Dashboard} from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+            <Dashboard/>
+        </header>
+    </div>
+);
+}
+
+export default App;
