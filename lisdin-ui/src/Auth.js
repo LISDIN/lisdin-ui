933618ef0c2bc7b626922e5530a46bb59c71b054
Mohammad Khan <mhrkhan13@gmail.com>
2022-06-11 12:45:48 -0400

    feat: ui for user profile setup form

---
+++ lisdin-ui/src/Auth.js
@@ -0,0 +1,31 @@
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import React from "react";
+
+Amplify.configure(awsmobile)
+
+export const AuthContext = React.createContext()
+
+export const AuthProvider = ({children}) => {
+    return (
+        <Authenticator 
+            variation="modal"
+            signUpAttributes={[
+                'given_name',
+                'family_name',
+                'preferred_username',
+                'address',
+
+            ]}
+            >
+                {({ signOut, user }) => (
+                    <AuthContext.Provider value={{ signOut, user }}>
+                        {children}
+                </AuthContext.Provider>
+                )}
+        </Authenticator>
+    )
+    
+}
