344de75ecd72c04d7af9c4d7f8bf8d2c981d146e
Mohammad Khan <mhrkhan13@gmail.com>
2022-06-15 20:58:19 -0400

    feat: navbar ui component

--- lisdin-ui/src/App.js
+++ lisdin-ui/src/App.js
@@ -1,23 +1,37 @@
 import {AuthProvider} from './Auth'
 import Profile from './Profile'
 import Home from './Home'
+import Ideas from './Ideas'
+import Account from './Account'
+import Collaborate from './Collaborate'
+import Messages from './Messages'
+import Notifications from './Notifications'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-        <AuthProvider>  
-            <Router>
-                <Routes>
-                    
-                    <Route exact path='/home' element={ <Home/>}/>
-                    <Route exact path="/" element={ <Profile/>}/>
-                </Routes>
-            </Router>
-            
+    <>
+        <CssBaseline/>
+        <div className="App">
+            <AuthProvider>  
+                <Router>
+                    <Routes>
+                        <Route exact path='/home' element={ <Home/>}/>
+                        <Route exact path='/ideas' element={ <Ideas/>}/>
+                        <Route exact path='/collaborate' element={ <Collaborate/>}/>
+                        <Route exact path='/messages' element={ <Messages/>}/>
+                        <Route exact path='/notifications' element={ <Notifications/>}/>
+                        <Route exact path='/account' element={ <Account/>}/>
+                        <Route exact path="/" element={ <Profile/>}/>
+                    </Routes>
+                </Router>
+                
 
-        </AuthProvider>        
-    </div>
+            </AuthProvider>        
+        </div>
+    </>
 );
 }
 
