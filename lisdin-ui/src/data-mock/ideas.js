86004c38f1bc2948bb014e279686f833a0441587
Mohammad Khan <mhrkhan13@gmail.com>
2022-07-03 14:53:50 -0400

    broke while celaning it up

---
+++ lisdin-ui/src/data-mock/ideas.js
@@ -0,0 +1,10 @@
+export const ideas = [
+    {
+        imageAlt : "groove moves",
+        imagePath : "Assets/lisdin-ideas/featured/groove-moves.svg",
+        title: "GrooveMove",
+        description: "A new way to work out",
+        likesCount : "0",
+        commentsCount: "0",
+    },
+]
